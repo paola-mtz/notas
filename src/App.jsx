a1a14db59ac36fee72a9fed1c92e042b00eeb775
paola-mtz <isc18350324@gmail.com>
2022-03-24 09:57:40 -0600

    Integracion de los estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
-
+import { useState } from "react";
 
 
 function App() {
   //TODOS: PRESENTAR EL CONCEPTO DE STATE
-  let varprueba ="hola"
-  const handleChangePrueba = (event) => { 
-    varprueba=event.target.value
-    
-   }
-  
 
+  //hooks
+  const [tituloState, settituloState]= useState("");
+  const [fechaState, setfechaState]= useState("");
+  const [notaState, setnotaState]= useState("");
+
+  const handleChangeTitulo = (event) => { 
+    settituloState(event.target.value);
+   }
+   const handleChangeFecha = (event) => { 
+    setfechaState(event.target.value);
+   }
+   const handleChangeNota = (event) => { 
+    setnotaState(event.target.value);
+   }
 
   return (
-    <div className="App" >
+    <div  className="App" >
+  
      <h3>Notas</h3>
-     <label htmlFor="PruebaID"> Input de Prueba</label>
+     <label htmlFor="Titulo"> Titulo</label>
      <input
-     id="PuebaID"
-     name="Prueba"
+     id="Titulo"
+     name="Titulo"
      type="text"
-     value={varprueba}
-     onChange={handleChangePrueba}/>
+     value={tituloState}
+     onChange={handleChangeTitulo}/>
+     <label htmlFor="Fecha"> Fecha</label>
+     <input
+     id="Fecha"
+     name="Fecha"
+     type="text"
+     value={fechaState}
+     onChange={handleChangeFecha}/>
+     <label htmlFor="Nota"> Nota</label>
+     <input
+     id="Nota"
+     name="Nota"
+     type="text"
+     value={notaState}
+     onChange={handleChangeNota}/>
     </div>
   );
 }
